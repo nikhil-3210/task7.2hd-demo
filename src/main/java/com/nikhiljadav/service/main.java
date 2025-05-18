d5cac24228b9ace673c9b22adec706eaec2dd344
Nikhil Jadav <nikhil.jadhav3210@gmail.com>
2025-05-18 16:18:44 +1000

    initial commit for task 7.2HD

---
+++ src/main/java/com/nikhiljadav/service/main.java
@@ -0,0 +1,8 @@
+package com.nikhiljadav.service;
+
+public class main {
+
+	public static void main(String[] args) {
+        System.out.println("App Engine Deployment Successful");
+	}
+}
---
+++ src/test/java/com/nikhiljadav/test/CalculatorTest.java
@@ -0,0 +1,26 @@
+package com.nikhiljadav.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.nikhiljadav.service.Calculator;
+
+public class CalculatorTest {
+	Calculator calculator = new Calculator();
+
+	@Test
+	public void testAddition() {
+		assertEquals(5, calculator.add(2, 3));
+	}
+
+	@Test
+	public void testSubtraction() {
+		assertEquals(1, calculator.subtract(3, 2));
+	}
+
+	@Test
+	public void testFailingAddition() {
+		assertEquals(10, calculator.add(4, 5));
+	} // intentional fail
+}
