bb4bee7ef3cc63a3cc8e5bb6140d5c75fda8a58f
Nikhil Jadav <nikhil.jadhav3210@gmail.com>
2025-05-18 16:21:45 +1000

    comments added

--- src/main/java/com/nikhiljadav/service/Calculator.java
+++ src/main/java/com/nikhiljadav/service/Calculator.java
@@ -1,6 +1,12 @@
 package com.nikhiljadav.service;
 
 public class Calculator {
-    public int add(int a, int b) { return a + b; }
-    public int subtract(int a, int b) { return a - b; }
+	// addition
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
 }
